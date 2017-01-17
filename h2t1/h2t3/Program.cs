0d9cbea348667ac6fd755329d5c0b04a300ef3fb
Asko Väänänen <H8688@student.jamk.fi>
2017-01-17 11:26:57 +0200

    lisättiin tehtävä 3

---
+++ h2t1/h2t3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h2t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku = 0;
+            Console.WriteLine("Anna rivimäärä > ");
+            luku = int.Parse(Console.ReadLine());
+            luku = luku - 2;
+
+
+
+            for (int laskuri = 0; laskuri < luku; laskuri++) // tulostetaan puu
+            {
+                for (int j = 0; j < luku-laskuri; j++)
+                {
+                    Console.Write(" "); // tässä tulee tyhjät välilyönnit puuhun
+
+                }
+
+
+            for (int tyhja_laskuri = 0; tyhja_laskuri < laskuri*2 + 1; tyhja_laskuri++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); // varsinaiset merkit
+
+
+            }
+            for (int j = 0; j < 2; j++)
+            {
+                    for (int l = 0; l < luku; l++)
+                    {
+                        Console.Write(" "); // jalan tyhjät lyönnit
+
+                    }
+
+
+                    for (int tyhja_laskuri = 0; tyhja_laskuri < 1; tyhja_laskuri++)
+                    {
+                        Console.Write("*"); // jalan merkit
+                    }
+                    Console.WriteLine();
+                }
+          
+        }
+    }
+}
+    
+
