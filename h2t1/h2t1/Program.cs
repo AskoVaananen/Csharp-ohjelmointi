729e7d46356203a76d7d53a192bd44a662348aac
Asko Väänänen <H8688@student.jamk.fi>
2017-01-17 10:11:06 +0200

    lisättiin

---
+++ h2t1/h2t1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h2t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pisteet = new int[5];
+            for (int i = 0; i < pisteet.Length; i++)
+            {
+                Console.WriteLine("Anna pisteet");
+                pisteet[i] = int.Parse(Console.ReadLine());
+            }
+            int summa = 0;
+            Array.Sort(pisteet);
+            for (int i = 1; i < pisteet.Length - 1; i++)
+            {
+
+               summa += (pisteet[i]);
+               
+            }
+
+            Console.Write("pisteet ovat = " + summa);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
