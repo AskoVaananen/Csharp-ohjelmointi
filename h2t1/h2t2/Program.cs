1e2119690c6d48ec3ded15e893ebdffd3327b448
Asko Väänänen <H8688@student.jamk.fi>
2017-01-17 11:03:07 +0200

    lisättiin tehtävä 2

---
+++ h2t1/h2t2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h2t2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arvosanat = { 0, 0, 0, 0, 0, 0 }; // alussa indexien summat on 0
+            
+            {
+                while (true) //kysyy niin pitkään kunnes antaa jonkun muun numeron kuin 0-5
+                {
+                    Console.WriteLine("Anna arvosana > ");
+                    int arvosana = int.Parse(Console.ReadLine());
+                    if (arvosana <= 5 && arvosana >= 0) // && = molemmat ehdot täyttyvät
+                    {
+                        arvosanat[arvosana]++;
+                    }
+                    else break;
+                }
+
+                for (int i = 0; i < arvosanat.Length; i++) // tulostaa arvosanat taulukon summat
+                {
+                    Console.Write(i);
+                    for (int k = 0; k < arvosanat[i]; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                }
+
+
+
+
+            }
+            
+            
+
+           
+
+          
+
+            
+           
+        }
+            
+    }
+
+
