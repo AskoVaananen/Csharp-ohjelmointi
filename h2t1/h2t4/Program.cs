70400816e7ca556bbc205163d8190a75d7c32699
Asko Väänänen <H8688@student.jamk.fi>
2017-01-17 13:26:59 +0200

    tehtävä 4 lisättiin

---
+++ h2t1/h2t4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h2t4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random oikea = new Random();
+            int oikea_vastaus = oikea.Next(1, 100);
+            int arvaus = 0;
+            int summa = 0;
+
+            Console.WriteLine("Arvaa luku 1 ja 100 väliltä > ");
+            arvaus = Convert.ToInt32(Console.ReadLine());
+
+            while (arvaus != oikea_vastaus)
+            {
+                summa++;
+                
+                if (arvaus < oikea_vastaus)
+                {
+                    Console.WriteLine("oikea arvaus on suurempi kuin arvaamasi numero  " + arvaus + " " + "arvaa uudestaan ");
+                    arvaus = Convert.ToInt32(Console.ReadLine());
+                }
+
+                else if (arvaus > oikea_vastaus)
+                {
+                    Console.WriteLine("oikea arvaus on pienempi kuin arvaamasi numero  " + arvaus + " " + "arvaa uudestaan ");
+                    arvaus = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+            
+            
+                Console.WriteLine("arvasit oikein! oikea vastaus oli" + " " + arvaus +"." + " " + "Arvasit " + summa + " " + "kertaa!");
+                
+                
+                Console.ReadLine();
+            }
+    }
+    
+        }
+        
+
