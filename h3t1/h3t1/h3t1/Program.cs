1115f08063c64096cce231b71b1a33d0c40b98c1
Asko Väänänen <H8688@student.jamk.fi>
2017-01-23 19:55:13 +0200

    Tehtävä 3 lisättiin

--- h3t1/h3t1/h3t1/Program.cs
+++ h3t1/h3t1/h3t1/Program.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ * Tehtävänäsi on ohjelmoida kiukaan toiminta.
+ * Kiuas pitää pystyä laittamaan päälle ja pois, sekä sen lämpötilaa että sen antamaa kosteutta pitää pystyä säätämään (arvoja ei ole rajattu).
+
+UML
+Suunnittele Kiuas-luokan ominaisuudet ja toiminnot UML-luokkakaaviona.
+
+Ohjelmointi
+Toteuta Kiuas-luokan ohjelmointi sekä pääohjelma, jolla luot olion Kiuas-luokasta.
+Säädä kiuas-oliota erilaisilla arvoilla, jätä Console.WriteLine()-tulostuslauseet ohjelmaasi, jotta kiuas-olion käyttäminen jää näkyville.
+ */
+
 
 namespace h3t1
 {
@@ -10,6 +22,29 @@ namespace h3t1
     {
         static void Main(string[] args)
         {
+            kiuas kiuas = new kiuas();
+            kiuas.IsOn = true;
+            kiuas.Temperature = 80;
+            kiuas.Humidity = 75;
+
+            //Console.WriteLine("kiuas status: {0}", kiuas.IsOn);
+            //Console.WriteLine("humidity: {0}", kiuas.Humidity);
+            //Console.WriteLine("temperature: {0}", kiuas.Temperature);
+
+            Console.WriteLine(kiuas.ToString());
+
+            kiuas.IsOn = false;
+            kiuas.Temperature = 50;
+            kiuas.Humidity = 50;
+
+            //Console.WriteLine("kiuas status: {0}", kiuas.IsOn);
+            //Console.WriteLine("humidity: {0}", kiuas.Humidity);
+            //Console.WriteLine("temperature: {0}", kiuas.Temperature);
+
+            Console.WriteLine(kiuas.ToString());
+            Console.ReadLine();
+
+
         }
     }
 }
---
+++ h3t1/h3t1/h3t1/kiuas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3t1
+{
+    class kiuas
+    {
+        public bool IsOn { get; set; }
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+
+
+        public override string ToString()
+        {
+            return "Kiuas status: " + IsOn + "\nTemperature: " + Temperature + "\nHumidity: " + Humidity + "\n";
+        }
+
+
+    }
+}
