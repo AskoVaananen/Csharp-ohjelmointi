179b5d25ba4758cda65e356cedfc160299dc009b
Asko Väänänen <H8688@student.jamk.fi>
2017-01-23 10:30:12 +0200

    Tehtävä 6 lisättiin

---
+++ h2t1/h2t6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h2t6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Console.WriteLine("Anna merkkijono: >"); // karvalakkimalli
+            string original = Console.ReadLine(); //otetaan merkkijono talteen
+            string reversed = new string(original.Reverse().ToArray()); // käännetään merkkijono toiseen jonoon tarkistusta varten
+            if (original == reversed) // jos on sama
+            {
+                
+                Console.WriteLine("Palindromi!" + " " + reversed);
+                Console.ReadLine(); 
+            }
+            else
+            {
+                Console.Write("Ei ole palindromi!");
+                Console.ReadLine();
+            } 
+        }
+    }
+}
