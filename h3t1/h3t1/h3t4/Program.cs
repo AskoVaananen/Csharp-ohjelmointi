0386872c4aa71d0f1eef98ab186e86f51781045e
Asko Väänänen <H8688@student.jamk.fi>
2017-01-24 11:21:22 +0200

    Lisättiin tehtävä 4

---
+++ h3t1/h3t1/h3t4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3t4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            vehicle vehicle = new vehicle();
+            Console.WriteLine("You created new vehicle" + vehicle.Name);
+
+            vehicle.Name = "Jeep";
+            vehicle.Speed = 100;
+            vehicle.Tyres = 4;
+
+            vehicle.PrintData();
+            Console.ReadLine();
+
+            vehicle.Accelerate();
+            vehicle.Accelerate();
+            vehicle.Brake();
+
+            Console.WriteLine(vehicle.ToString());
+            Console.ReadLine();
+
+            vehicle bus = new vehicle();
+            Console.WriteLine("You created new vehicle");
+            bus.Name = "Volvo";
+            bus.Speed = 40;
+            bus.Tyres = 10;
+
+            bus.PrintData();
+            Console.ReadLine();
+            bus.Accelerate();
+            bus.Brake();
+
+            Console.WriteLine(bus.ToString());
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ h3t1/h3t1/h3t4/vehicle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3t4
+{
+    class vehicle
+    {
+
+        // properties
+        public string Name
+        {
+            get;
+            set;
+        }
+
+
+        public int Speed
+        {
+            get;
+            set;
+        }
+
+        public int Tyres
+        {
+            get;
+            set;
+        }
+
+
+        public void Accelerate()
+        {
+            Speed += 10;
+            Console.WriteLine(" You increased speed + 10 units\n" + " Speed is now: " + Speed);
+            
+
+        }
+        // methods
+        public void Brake()
+        {
+            Speed -= 10;
+            Console.WriteLine(" You lowered speed - 10 units\n" + " Speed is now:" + Speed);
+        }
+
+
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle data : ");
+            Console.WriteLine("- Name : " + Name);
+            Console.WriteLine("- Tyres : " + Tyres);
+            Console.WriteLine("- Speed : " + Speed);
+
+
+        }
+
+        public override string ToString()
+        {
+            return "Vehicle name: " + Name + "\nTyres: " + Tyres + "\nSpeed: " + Speed;
+
+        }
+
+
+    }
+}
