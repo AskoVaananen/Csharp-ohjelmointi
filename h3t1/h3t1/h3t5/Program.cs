ef0fdd2908bb8310a6f114232e5c92f8f26659f0
Asko Väänänen <H8688@student.jamk.fi>
2017-01-24 12:32:20 +0200

    Lisättiin tehtävä 5

---
+++ h3t1/h3t1/h3t5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3t5
+{
+
+    /*
+     * Suunnittele UML-editorilla Opiskelija-luokka, joka sisältää opiskelijalle tyypillisiä tietoja ja toimintoja. 
+     * Tee uusi projekti. Ohjelmoi Opiskelija-luokka sekä toteuta pääohjelma, joka luo muutamia opiskelijoita ja tallentaa opiskelijat taulukkoon
+     * (esim. 5 opiskelijaa). Tulosta taulukossa olevien opiskelijoiden tiedot käyttämällä toistorakennetta. 
+
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student[] students = new student[5];
+            students[0] = new student("Asko", "Väänänen", "H8688", "TTV15S6", 22);
+            students[1] = new student("Matti", "Kohonen", "H8628", "TTV15S6", 21);
+            students[2] = new student("Jesse", "Siekkinen", "H1234", "TTV15S6", 20);
+            students[3] = new student("Aatu", "Muukka", "K2288", "TTV15S6", 19);
+            students[4] = new student("Ville", "Vahtokari", "H3456", "TTV15S6", 88);
+
+    
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i].Printdata();
+            }
+
+            Console.ReadLine();
+            Console.WriteLine("Happy Birthdat Jesse!\n");
+            students[2].IncreaseAge();
+
+            Console.WriteLine("Happy Birthdat Aatu!\n");
+            students[3].IncreaseAge();
+
+            Console.ReadLine();
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i].Printdata();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ h3t1/h3t1/h3t5/student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3t5
+{
+    class student
+    {
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string StudentId
+        {
+            get;
+            set;
+        }
+        public string Group
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public void Printdata()
+        {
+            Console.WriteLine(" Student information : ");
+            Console.WriteLine("- Name : " + FirstName);
+            Console.WriteLine("- Lastname : " + LastName);
+            Console.WriteLine("- Student ID : " + StudentId);
+            Console.WriteLine("- Group : " + Group);
+            Console.WriteLine("- Age : " + Age);
+            Console.WriteLine();
+
+
+
+        }
+
+
+        public void IncreaseAge()
+        {
+            Age += 1;
+            Console.WriteLine("Age increased 1 unit \n");
+
+        }
+
+        public student(string firstname, string lastname, string studentid, string group, int age)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            StudentId = studentid;
+            Group = group;
+            Age = age;
+
+        }
+
+
+
+    }
+}
