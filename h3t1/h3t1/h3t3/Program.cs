6bfd2fadbe662a3c339d8aed5ad940f828723911
Asko Väänänen <H8688@student.jamk.fi>
2017-01-24 10:17:01 +0200

    Lisättiin tehtävä 3

---
+++ h3t1/h3t1/h3t3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            television television = new television();
+
+            television.IsOn = true;
+            television.Volume = 0;
+            television.Channel = 0;
+
+            Console.WriteLine(television.ToString());
+            Console.ReadLine();
+
+            television.AdjustVolumeUp();
+            television.AdjustVolumeUp();
+            television.AdjustVolumeUp();
+
+            Console.WriteLine(television.ToString());
+            Console.ReadLine();
+
+            television.AdjustVolumeDown();
+            television.AdjustVolumeDown();
+            television.AdjustVolumeDown();
+
+            Console.WriteLine(television.ToString());
+            Console.ReadLine();
+
+            television.ChangeChannelUp();
+            television.ChangeChannelUp();
+
+            television.ChangeChannelDown();
+            television.IsOn = false;
+
+            Console.WriteLine(television.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ h3t1/h3t1/h3t3/television.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3t3
+{
+    class television
+    {
+        public bool IsOn { get; set; }
+        public int Volume { get; set; }
+        public int Channel { get; set; }
+
+        public void AdjustVolumeUp()
+        {
+            Volume += 1;
+
+        }
+
+        public void ChangeChannelUp()
+        {
+            Channel += 1;
+
+        }
+        public void AdjustVolumeDown()
+        {
+            Volume -= 1;
+
+        }
+
+        public void ChangeChannelDown()
+        {
+            Channel -= 1;
+
+        }
+
+        public override string ToString()
+        {
+            return "Television status: " + IsOn + "\nChannel: " + Channel + "\nVolume: " + Volume;
+
+        }
+
+    }
+}
