bcdce996023c31744f3af73d3d0b383049910b95
Asko Väänänen <H8688@student.jamk.fi>
2017-01-24 09:52:36 +0200

    Lisättiin tehtävä 2

---
+++ h3t1/h3t1/h3t2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3t2
+{
+    /*
+     * Tehtävänäsi on ohjelmoida pesukoneen toiminta. Samoin kuin edellinen tehtävä: mitä ominaisuuksia ja toimintoja tekisit Pesukone-luokkaan?
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            washingmachine washingmachine = new washingmachine();
+
+            washingmachine.IsOn = true;
+            washingmachine.Watertemperature = 60;
+            washingmachine.Wateron = true;
+            washingmachine.Sling = 600;
+
+            Console.WriteLine(washingmachine.ToString());
+            Console.ReadLine();
+
+            washingmachine.IsOn = false;
+            washingmachine.Watertemperature = 20;
+            washingmachine.Wateron = false;
+            washingmachine.Sling = 200;
+
+            Console.WriteLine(washingmachine.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ h3t1/h3t1/h3t2/washingmachine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h3t2
+{
+    class washingmachine
+    {
+        public bool IsOn { get; set; }
+        public int Watertemperature { get; set; }
+        public int Sling { get; set; }
+        public bool Wateron { get; set; }
+
+        public override string ToString()
+        {
+            return "washingmachine status: " + IsOn + "\nTemperature: " + Watertemperature + "\nSling: " + Sling + " Rounds/minute" + "\n" +"Water status: " + Wateron;
+
+        }
+    }
+}
