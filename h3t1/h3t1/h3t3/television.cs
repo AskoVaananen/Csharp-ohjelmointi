8008d0227f6cf1f012d0b095a081d20368225c36
Asko Väänänen <H8688@student.jamk.fi>
2017-01-24 10:46:10 +0200

    Muutettin tehtävän 3 tulostusta erilaiseksi

--- h3t1/h3t1/h3t3/television.cs
+++ h3t1/h3t1/h3t3/television.cs
@@ -8,31 +8,40 @@ namespace h3t3
 {
     class television
     {
-        public bool IsOn { get; set; }
-        public int Volume { get; set; }
-        public int Channel { get; set; }
+        public bool IsOn { get;
+                            set; }
+
+
+        public int Volume { get;
+                            set; }
+
+        public int Channel { get;
+                             set; }
 
         public void AdjustVolumeUp()
         {
             Volume += 1;
+            Console.WriteLine("Volume adjusted one level higher");
 
         }
 
         public void ChangeChannelUp()
         {
             Channel += 1;
+            Console.WriteLine("Channel adjusted one level higher");
 
         }
         public void AdjustVolumeDown()
         {
             Volume -= 1;
+            Console.WriteLine("Volume adjusted one level lower");
 
         }
 
         public void ChangeChannelDown()
         {
             Channel -= 1;
-
+            Console.WriteLine("Channel adjusted one level lower");
         }
 
         public override string ToString()
