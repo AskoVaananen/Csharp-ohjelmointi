0a3c54c5a4dee2c1fa3ba43ea1846e2cc0b3e2c2
Asko Väänänen <H8688@student.jamk.fi>
2017-01-31 10:14:30 +0200

    Lisättiin tehtävä 2

---
+++ h4t1/h4t1/h4t2/Amplifier.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h4t2
+{
+    class Amplifier
+    {
+
+        private const int minVolume = 1;
+        private const int maxVolume = 100;
+        private int volume = 1;
+
+        public int Volume
+        {
+
+            get
+            {
+                return volume;
+            }
+
+
+            set
+            {
+                
+
+                    if (value < minVolume)
+                    {
+
+                    Console.WriteLine("volume level is too low, set to minimum");
+                    volume = minVolume;
+
+                    }
+
+                    else if (value > maxVolume)
+
+                    {
+
+                    Console.WriteLine("volume level is too high, set to maximum");
+                    volume = maxVolume;
+
+                    }
+
+                    else
+                    {
+                        volume = value;
+                    }
+                }
+            }
+        }
+    }
+
+
+    
+
---
+++ h4t1/h4t1/h4t2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h4t2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Amplifier amplifier = new Amplifier();
+            bool result;
+
+            do
+            {
+                Console.WriteLine("Volume set to" + " " + amplifier.Volume + " dB");
+                Console.WriteLine("Set volume level: (0-100) >");
+
+                string line = Console.ReadLine();
+                int volume;
+                result = int.TryParse(line, out volume);
+                if (result)
+                {
+                    amplifier.Volume = volume;
+                }
+
+            }
+            while (result);
+            Console.WriteLine("press something");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
