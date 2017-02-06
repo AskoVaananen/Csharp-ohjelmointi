445ff99fff95436cc7b8f3ef994f3c66375aa43a
Asko Väänänen <H8688@student.jamk.fi>
2017-02-06 19:58:00 +0200

    created

--- h5t1/h5t1/Program.cs
+++ h5t1/h5t1/Program.cs
@@ -10,6 +10,21 @@ namespace h5t1
     {
         static void Main(string[] args)
         {
+            Persons myFriends = new Persons();
+            Person friend1 = new Person { Firstname = "Kirsi", Lastname = "Mainio", SocialSecurityNumber = "010190-111A" };
+            Person friend2 = new Person{ Firstname = "Matti", Lastname = "Mainio", SocialSecurityNumber = "010190-111A" };
+            Person friend3 = new Person{ Firstname = "Husso", Lastname = "Mainio", SocialSecurityNumber = "010190-111A" };
+
+            myFriends.AddPerson(friend1);
+
+            myFriends.AddPerson(friend2);
+
+            myFriends.AddPerson(friend3);
+
+            myFriends.PrintData();
+
+            Person friend5 = myFriends.FindPerson("010190-111A");
+            if (friend5 != null) Console.WriteLine(friend5);
         }
     }
 }
