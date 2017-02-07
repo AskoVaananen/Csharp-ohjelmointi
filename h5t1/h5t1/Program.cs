cdea61f30e95236bbadc80a58763cf67cbaaa1ad
Asko Väänänen <H8688@student.jamk.fi>
2017-02-07 14:04:00 +0200

    tehtävät 1 ja 3

--- h5t1/h5t1/Program.cs
+++ h5t1/h5t1/Program.cs
@@ -23,8 +23,13 @@ namespace h5t1
 
             myFriends.PrintData();
 
+            Person friend4 = myFriends.GetPerson(2); // etsii indexin perusteella, tässä tapauksessa husso on 2 indexi peronsissa
+            if (friend4 != null) Console.WriteLine(friend4);
+
             Person friend5 = myFriends.FindPerson("010190-111A");
             if (friend5 != null) Console.WriteLine(friend5);
+
+            myFriends.PrintData();
         }
     }
 }
