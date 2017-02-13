12e1f729cc0220213ce0be10490cf9f7b9087611
Asko Väänänen <H8688@student.jamk.fi>
2017-02-13 13:28:03 +0200

    Testings

---
+++ h5t1/h5t3_uusi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h5t3_uusi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            yritys henkilot = new yritys();
+
+            while (true)
+            {
+                int valikko = 0;
+                Console.WriteLine("1. Palkkaa. 2. Erota. 3. Listaa 0. Lopeta:");
+                Console.WriteLine("Give a selection");
+                bool status = int.TryParse(Console.ReadLine(), out valikko);
+
+                switch (valikko)
+
+
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+
+                        Console.WriteLine("Give a new employee");
+                        string name = Console.ReadLine();
+                        henkilot.AddEmployee(name);
+                        break;
+
+
+                    case 2:
+
+                        Console.WriteLine("Delete latest employee");
+                        henkilot.DelEmployee();
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("List all employees");
+                        henkilot.PrintData();
+                        break;
+
+                }
+            }
+        }
+    }
+}
+
---
+++ h5t1/h5t3_uusi/yritys.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h5t3_uusi
+{
+    class yritys
+    {
+        Stack<string> henkilot = new Stack<string>();
+
+
+
+        public void AddEmployee(string name)
+        {
+            henkilot.Push(name);
+
+        }
+        public void DelEmployee()
+        {
+
+            string temp = (henkilot.Pop()); // third
+        }
+        public void PrintData()
+        {
+
+            // print all
+            foreach (string s in henkilot)
+            {
+                Console.WriteLine(s);
+
+                //string temp = henkilot.Pop();
+                //Console.WriteLine("temp is {0}", temp); // temp is third
+                //Console.WriteLine("Count is {0}", henkilot.Count); // count is 2
+
+
+
+
+            }
+        }
+    }
+}
