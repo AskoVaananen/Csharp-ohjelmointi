7c7cf1d3bfdfd865c80e2706cf36ca8126739824
Asko Väänänen <H8688@student.jamk.fi>
2017-02-14 11:25:20 +0200

    t2

---
+++ h6t1/h6t1/h6t2/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h6t1
+{
+    class Car
+    {
+
+        public int Count
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+
+        public Car(string name)
+        {
+            Name = name;
+            Count = 0;
+
+        }
+   
+        
+        public void AddCar()
+        {
+            Count++;
+
+        }
+
+    }
+}
---
+++ h6t1/h6t1/h6t2/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace h6t1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+       private Car car;
+       private Car truck;
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.car = new Car("Audi");
+            Audi.Content = car.Name;
+
+            this.truck = new Car("Sisu");
+            Sisu.Content = truck.Name;
+ 
+        }
+
+        private void TruckButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            truck.AddCar();
+            TruckTextBlock.Text = truck.Count.ToString();
+
+
+        }
+
+        private void CarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            car.AddCar();
+            CarTextBlock.Text = car.Count.ToString();
+
+
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            CarTextBlock.Text = "0";
+            TruckTextBlock.Text = "0";
+        }
+    }
+}
+
