977f062a317433258310cbb9cbaef7b0eeba70c1
Asko Väänänen <H8688@student.jamk.fi>
2017-03-06 15:36:40 +0200

    Tehtävä 7 - 1 aloitettu

---
+++ h7t1/h7t1/Henkilo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h7t1
+{
+    class Henkilo
+    {
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ h7t1/h7t1/Yritys.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h7t1
+{
+    class Yritys
+    {
+        Stack<string> henkilot = new Stack<string>();
+
+
+
+        public void AddEmployee(string name)
+        {
+            henkilot.Push(name);
+
+        }
+        public void DelEmployee()
+        {
+
+            string temp = (henkilot.Pop()); // third
+        }
+        public void PrintData()
+        {
+
+            // print all
+            foreach (string s in henkilot)
+            {
+                System.IO.StreamWriter outputFile = new System.IO.StreamWriter("test.txt");
+                outputFile.WriteLine("Here is a sample text to file.");
+                outputFile.Close();
+                Console.WriteLine(s);
+
+                //string temp = henkilot.Pop();
+                //Console.WriteLine("temp is {0}", temp); // temp is third
+                //Console.WriteLine("Count is {0}", henkilot.Count); // count is 2
+
+
+
+
+            }
+        }
+    }
+}
+}
